2ffbc1623f6560a69e38e856b0776c087a5ddca4
Wandalen <wandalen.me@gmail.com>
2018-12-10 09:09:10 +0200

    auto commit

--- proto/dwtools/amid/l5_mapper.test/PathTranslator.test.s
+++ proto/dwtools/amid/l5_mapper.test/PathTranslator.test.s
@@ -2,30 +2,10 @@
 
 'use strict';
 
-var isBrowser = true;
-
 if( typeof module !== 'undefined' )
 {
-  isBrowser = false;
 
-  if( typeof _global_ === 'undefined' || !_global_.wBase )
-  {
-    let toolsPath = '../../../dwtools/Base.s';
-    let toolsExternal = 0;
-    try
-    {
-      require.resolve( toolsPath );
-    }
-    catch( err )
-    {
-      toolsExternal = 1;
-      require( 'wTools' );
-    }
-    if( !toolsExternal )
-    require( toolsPath );
-  }
-
-  var _ = _global_.wTools;
+  let _ = require( '../../Tools.s' );
 
   _.include( 'wTesting' );
 
